f16c3ae32f4f343946ea462ac0896260362da16b
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-02-07 16:05:42 +0200

    Add translation status and synonyms to HPO
    fixes #21

--- src/babelon/parsers/xliff.py
+++ src/babelon/parsers/xliff.py
@@ -23,13 +23,29 @@ class XliffParser:
         csvfile = open(self.output_file, 'w', encoding='utf-8')
         csvfile_writer = csv.writer(csvfile, delimiter='\t')
 
+        csvfile_synonyms = open(self.output_file+"_synonyms.tsv", 'w', encoding='utf-8')
+        csvfile_writer_synonyms = csv.writer(csvfile_synonyms, delimiter='\t')
+
         # ADD HEADER
         csvfile_writer.writerow(
             ['source_language', 'translation_language', 'subject_id', 'predicate_id', 'source_value',
-             'translation_value'])
+             'translation_value', 'translation_status'])
+        csvfile_writer_synonyms.writerow(
+            ['subject_id', 'translation_value', 'comment'])
+        
+        lang = "unknown"
+
+        if doc['xliff']['file']['body']['trans-unit']:
+            lang = doc['xliff']['file']['body']['trans-unit'][0]['target']['@xml:lang']
+
+        csvfile_writer_synonyms.writerow(
+            ['ID', f'AL oboInOwl:hasExactSynonym@{lang}', ''])
+        
+
         for trans_unit in doc['xliff']['file']['body']['trans-unit']:
             source_language = trans_unit['source']['@xml:lang']
             translation_language = trans_unit['target']['@xml:lang']
+            translation_status = self.get_translation_status(trans_unit['target']['@state'])
             # get subject_id and and format
             split_string_id_column = trans_unit['@id']
             get_subject_id = split_string_id_column[0: 10]
@@ -45,10 +61,42 @@ class XliffParser:
                 predicate_id = get_predicate_id.replace('synonyms', 'oboInOwl:exactSynonym')
             source_value = trans_unit['source']['#text']
             translational_value = trans_unit['target']['#text']
-            csv_line = [source_language, translation_language, subject_id, predicate_id, source_value,
-                        translational_value]
-            csvfile_writer.writerow(csv_line)
+            
+            csv_line = []
+            
+            if predicate_id=="oboInOwl:exactSynonym":
+                all_synonyms = self.synonym_split_value(translational_value)
+                subject_id = subject_id.replace('\n', ' ').replace('\r', '').strip()
+                translation_status = translation_status.replace('\n', ' ').replace('\r', '').strip()
+
+                for syn in all_synonyms:
+                    csv_line = [subject_id, syn, translation_status]
+                    csvfile_writer_synonyms.writerow(csv_line)
+            else:
+                for l in [source_language, translation_language, subject_id, predicate_id, source_value,
+                        translational_value, translation_status]:
+                    csv_line.append(l.replace('\n', ' ').replace('\r', '').strip())
+                csvfile_writer.writerow(csv_line)
+
         return self.output_file
+
+    def get_translation_status(self, translation_status_raw):
+        if translation_status_raw:
+            if translation_status_raw=="needs-translation":
+                return "NOT_TRANSLATED"
+            elif translation_status_raw=="final":
+                return "OFFICIAL"
+            elif translation_status_raw=="translated":
+                return "CANDIDATE"
+            else:
+                return translation_status_raw
+        return ""
+
+    def synonym_split_value(self, raw_synonym):
+        word_list = raw_synonym.split('#')
+        stripped = [s.replace('\n', ' ').replace('\r', '').strip() for s in word_list if s]
+        return stripped
+    
 
     def synonym_split(self):
         df = pd.read_csv(self.output_file, sep='\t')
